3e24f0afec3047cd4ef7575e1eba0857e4ef6cf6
chetasr <me@chetasr.co>
2020-09-26 16:50:26 +0400

    Add colabUtils

---
+++ colabUtils/__init__.py
@@ -0,0 +1,10 @@
+# colab project template
+class colabApp:
+    def __init__(self):
+        raise NotImplementedError
+
+    def start(self):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
---
+++ main.py
@@ -0,0 +1,15 @@
+import colabUtils
+
+#user code here
+class ExampleApp(colabUtils.colabApp):
+    def __init__(self):
+        # TODO
+        raise NotImplementedError
+
+    def start(self):
+        # TODO
+        raise NotImplementedError
+
+    def stop(self):
+        # TODO
+        raise NotImplementedError
