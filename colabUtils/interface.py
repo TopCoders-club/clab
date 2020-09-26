0e3e891f1ad228845a86de1280fd3cc23e6aabad
chetasr <thephantomblu@gmail.com>
2020-09-26 19:43:01 +0400

    Add ColabSFTPClient

--- colabUtils/interface.py
+++ colabUtils/interface.py
@@ -6,6 +6,24 @@ import subprocess
 import paramiko
 import os
 
+class ColabSFTPClient(paramiko.SFTPClient):
+    def put_dir(self, source, target):
+        for item in os.listdir(source):
+            if os.path.isfile(os.path.join(source, item)):
+                self.put(os.path.join(source, item), '%s/%s' % (target, item))
+            else:
+                self.mkdir('%s/%s' % (target, item), ignore_existing=True)
+                self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
+
+    def mkdir(self, path, mode=511, ignore_existing=False):
+        try:
+            super(ColabSFTPClient, self).mkdir(path, mode)
+        except IOError:
+            if ignore_existing:
+                pass
+            else:
+                raise
+
 config_file = '../colab.yaml'
 
 def get_ngrok_id():
