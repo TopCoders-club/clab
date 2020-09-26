93daa5b970aed26bf26ef10e5e3c9daa15bcc46a
tirth2212 <52685414+tirth2212@users.noreply.github.com>
2020-09-26 19:38:11 +0530

    selenium.py

---
+++ selenium.py
@@ -0,0 +1,11 @@
+def openurl_function():
+    from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
+    from selenium.webdriver.common.keys import Keys
+
+    from selenium import webdriver
+    driver = webdriver.Firefox()
+    driver.get('http://www.google.com')
+
+
+openurl_function()
