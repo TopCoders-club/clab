a0e18b01191d860496e21b73dacba9e3d9bab2eb
chetasr <thephantomblu@gmail.com>
2020-09-27 13:58:15 +0400

    Change cola to clab

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "cola",
+    name = "clab",
     version = "0.1",
     packages=find_packages(),
     url = "https://github.com/dvlp-jrs/shellhacks2020",
     author = "code200",
     install_requires = ["fabric", "PyInquirer", "pyyaml", "pyngrok", "halo"],
     entry_points = {
-        'console_scripts': ['cola=colabUtils.interface:main'],
+        'console_scripts': ['clab=colabUtils.interface:main'],
     }
 )
