0a82adf2a5b32852c5fe6a2018dbe36c7044b239
chetasr <thephantomblu@gmail.com>
2020-09-27 13:58:32 +0400

    Update author

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1",
     packages=find_packages(),
     url = "https://github.com/dvlp-jrs/shellhacks2020",
-    author = "code200",
+    author = "topcoders-club",
     install_requires = ["fabric", "PyInquirer", "pyyaml", "pyngrok", "halo"],
     entry_points = {
         'console_scripts': ['clab=colabUtils.interface:main'],
